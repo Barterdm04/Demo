bde3a383ec0186a552f80e91398027b59085c771
barterdm04 <dbarter1@my.wctc.edu>
2016-02-09 16:10:22 -0600

    Remove Images as well as CustomerNumber from bill to drop down

---
+++ Insite.NEB/InSiteCommerce.Web/Scripts/Custom/App/account/custom.selectcustomer.controller.ts
@@ -0,0 +1,25 @@
+﻿module insite.account {
+    "use strict";
+
+    export class CustomSelectCustomerController extends SelectCustomerController{
+
+        
+    }
+
+    angular
+        .module("insite")
+        .controller("SelectCustomerController", SelectCustomerController)
+        .filter('removeSequences', function() {
+            return function(items) {
+                var filtered = [];
+                angular.forEach(items, function(item) {
+                    if (item.label) {
+                        item.label = item.label.replace(item.customerNumber, '');
+                    }
+
+                    filtered.push(item);
+                });
+                return filtered;
+            };
+        });
+}
